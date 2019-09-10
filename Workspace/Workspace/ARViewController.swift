095ed8b2d7ffcce9de6a05637bc7e49ef38fcb6b
jamestouri <j_touri@yahoo.com>
2019-09-09 21:00:18 -0700

    Add Xcode project

---
+++ Workspace/Workspace/ARViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Workspace
+//
+//  Created by James touri on 9/9/19.
+//  Copyright © 2019 jamestouri. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ARViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Workspace/WorkspaceTests/WorkspaceTests.swift
@@ -0,0 +1,34 @@
+//
+//  WorkspaceTests.swift
+//  WorkspaceTests
+//
+//  Created by James touri on 9/9/19.
+//  Copyright © 2019 jamestouri. All rights reserved.
+//
+
+import XCTest
+@testable import Workspace
+
+class WorkspaceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
