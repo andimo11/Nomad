91bf564f3424684501cd4535b8878f33daca23e8
jamestouri <j_touri@yahoo.com>
2019-09-17 18:02:01 -0700

    begin voice assistant

---
+++ Workspace/Workspace/VoiceAssistantViewController.swift
@@ -0,0 +1,33 @@
+//
+//  VoiceAssistantViewController.swift
+//  Workspace
+//
+//  Created by James touri on 9/17/19.
+//  Copyright © 2019 jamestouri. All rights reserved.
+//
+
+import UIKit
+
+class VoiceAssistantViewController: UIViewController {
+    
+    let APIKey = "AIzaSyD7RpDRF15KsNnvv_Poln8oqVBp5-VP_ZI"
+    let apiUrl = "https://speech.googleapis.com/v1/operations/"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
