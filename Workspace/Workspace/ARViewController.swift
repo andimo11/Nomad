118e666c53696a6c8dbe88db47a31188937597fe
andimop11 <andimop11@gmail.com>
2019-11-02 11:12:12 -0700

    fixed increment

--- Workspace/Workspace/ARViewController.swift
+++ Workspace/Workspace/ARViewController.swift
@@ -72,69 +72,71 @@ class ARViewController: UIViewController, UIWebViewDelegate {
         
         //need to remove nodes
     }
+    
+        @IBAction func incrementButton(_ sender: Any) {
+            if counter <= 5 {
+                counter += 1
+                self.numberOfScreens.text = String(counter)
+    
+            }
+            let rect = CGRect(x: 40, y: 80, width: 400, height: 400)
+            var webView: UIWebView! = UIWebView(frame: rect)
+    
+            DispatchQueue.main.async {
+                //creates webView node
+                self.view.addSubview(webView!)
+    
+                //creates the plane where the screen will be displayed
+                let displayPlane = SCNPlane(width: 0.5,height: 0.3)
+    
+    //James, ignore this
+    //**********//Trying to initialize plane from custom screen**********
+    //            let displayPlane = SCNScene(named: "SceneKit Asset Catalog.scnassets/SceneKit Scene.scn")
+                //^ top replaced bottom v
+    
+                let webUrl : NSURL = NSURL(string: "https://google.com")!
+                let request : NSURLRequest = NSURLRequest(url: webUrl as URL)
+    
+                webView.loadRequest(request as URLRequest)
+    
+                //projects the contents of the webView onto the plane
+                displayPlane.firstMaterial?.diffuse.contents = webView
+    
+                //creates nodeø
+                let webScreen = SCNNode(geometry: displayPlane)
+    
+                //puts screen where camera is facing
+                let cc = self.getCameraCoordinates(sceneView: self.sceneView)
+    
+                //places the screen where the camera is facing, z axis is altered to push screen back
+                webScreen.position = SCNVector3(cc.x, cc.y, cc.z - 0.75)
+    
+                self.sceneView.scene.rootNode.addChildNode(webScreen)
+    
+            }
+    
+        }
+        
+//    call this function to get current location * other transformation code
+        func getCameraCoordinates(sceneView: ARSCNView) -> myCameraCoordinates {
+            let cameraTransform = sceneView.session.currentFrame?.camera.transform
+            let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
+    
+            var cc = myCameraCoordinates()
+            cc.x = cameraCoordinates.translation.x
+            cc.y = cameraCoordinates.translation.y
+            cc.z = cameraCoordinates.translation.z
+    
+            return cc
+        }
+
+//    variables with view location data
+    struct myCameraCoordinates {
+        var x = Float()
+        var y = Float()
+        var z = Float()
+    }
 }
 
 //***not being used***
-//    @IBAction func incrementButton(_ sender: Any) {
-//        if counter <= 5 {
-//            counter += 1
-//            self.numberOfScreens.text = String(counter)
-//
-//        }
-//        let rect = CGRect(x: 40, y: 80, width: 400, height: 400)
-//        var webView: UIWebView! = UIWebView(frame: rect)
-//
-//        DispatchQueue.main.async {
-//            //creates webView node
-//            self.view.addSubview(webView!)
-//
-//            //creates the plane where the screen will be displayed
-//            let displayPlane = SCNPlane(width: 0.5,height: 0.3)
-//
-////James, ignore this
-////**********//Trying to initialize plane from custom screen**********
-////            let displayPlane = SCNScene(named: "SceneKit Asset Catalog.scnassets/SceneKit Scene.scn")
-//            //^ top replaced bottom v
-//
-//            let webUrl : NSURL = NSURL(string: "https://google.com")!
-//            let request : NSURLRequest = NSURLRequest(url: webUrl as URL)
-//
-//            webView.loadRequest(request as URLRequest)
-//
-//            //projects the contents of the webView onto the plane
-//            displayPlane.firstMaterial?.diffuse.contents = webView
-//
-//            //creates nodeø
-//            let webScreen = SCNNode(geometry: displayPlane)
-//
-//            //puts screen where camera is facing
-//            let cc = self.getCameraCoordinates(sceneView: self.sceneView)
-//
-//            //places the screen where the camera is facing, z axis is altered to push screen back
-//            webScreen.position = SCNVector3(cc.x, cc.y, cc.z - 0.75)
-//
-//            self.sceneView.scene.rootNode.addChildNode(webScreen)
-//
-//        }
-//
-//    }
-    
-//call this function to get current location * other transformation code
-//    func getCameraCoordinates(sceneView: ARSCNView) -> myCameraCoordinates {
-//        let cameraTransform = sceneView.session.currentFrame?.camera.transform
-//        let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
-//
-//        var cc = myCameraCoordinates()
-//        cc.x = cameraCoordinates.translation.x
-//        cc.y = cameraCoordinates.translation.y
-//        cc.z = cameraCoordinates.translation.z
-//
-//        return cc
-//    }
 
-//variables with view location data
-//struct myCameraCoordinates {
-//    var x = Float()
-//    var y = Float()
-//    var z = Float()
-//}
