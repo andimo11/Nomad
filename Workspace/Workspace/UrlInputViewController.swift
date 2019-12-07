46071c241688d1c22c51662789a25d5393b947e4
jamestouri <j_touri@yahoo.com>
2019-12-06 18:39:12 -0800

    Transfer URLs through segue

--- Workspace/Workspace/UrlInputViewController.swift
+++ Workspace/Workspace/UrlInputViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class UrlInputViewController: UIViewController {
 
+    @IBOutlet var urlInputFields: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,8 +24,14 @@ class UrlInputViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "transferUrls") {
+            var urlTexts: [String] = []
             let ARViewController = segue.destination as! ARViewController
-            
+            for urlField in urlInputFields {
+                if urlField.text != "" {
+                    urlTexts.append("https://" + urlField.text!)
+                }
+            }
+            ARViewController.inputUrls = urlTexts
         }
     }
 
